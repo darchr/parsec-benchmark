81a4019d891516643e0a1b0912ff1ff9d3b45a69
mahyarsamani <msamani@ucdavis.edu>
2020-02-07 17:31:04 -0800

    Added the install.sh file and changed get-inputs to be able to download the native inputs as well.

---
+++ install.sh
@@ -0,0 +1,14 @@
+soruce env.sh
+parsecmgmt -a build -p blackscholes -c gcc-hooks 
+parsecmgmt -a build -p bodytrack -c gcc-hooks 
+parsecmgmt -a build -p canneal -c gcc-hooks 
+parsecmgmt -a build -p dedup -c gcc-hooks 
+parsecmgmt -a build -p facesim -c gcc-hooks 
+parsecmgmt -a build -p ferret -c gcc-hooks 
+parsecmgmt -a build -p fluidanimate -c gcc-hooks 
+parsecmgmt -a build -p freqmine -c gcc-hooks 
+parsecmgmt -a build -p streamcluster -c gcc-hooks
+parsecmgmt -a build -p swaptions -c gcc-hooks 
+parsecmgmt -a build -p vips -c gcc-hooks 
+parsecmgmt -a build -p x264 -c gcc-hooks 
+parsecmgmt -a build -p raytrace -c gcc-hooks 
