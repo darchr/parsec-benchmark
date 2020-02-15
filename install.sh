a326e01674a275fffe5057acba66cc559b81740b
mahyarsamani <msamani@ucdavis.edu>
2020-02-14 19:16:17 -0800

    Updated instal.sh.

--- install.sh
+++ install.sh
@@ -1,14 +1,33 @@
-soruce env.sh
+source env.sh
+parsecmgmt -a build -p blackscholes 
 parsecmgmt -a build -p blackscholes -c gcc-hooks 
+parsecmgmt -a build -p bodytrack 
 parsecmgmt -a build -p bodytrack -c gcc-hooks 
-parsecmgmt -a build -p canneal -c gcc-hooks 
-parsecmgmt -a build -p dedup -c gcc-hooks 
+parsecmgmt -a build -p canneal 
+parsecmgmt -a build -p canneal -c gcc-hooks
+parsecmgmt -a build -p dedup 
+parsecmgmt -a build -p dedup -c gcc-hooks  
+parsecmgmt -a build -p netdedup 
+parsecmgmt -a build -p netdedup -c gcc-hooks 
+parsecmgmt -a build -p facesim 
 parsecmgmt -a build -p facesim -c gcc-hooks 
+parsecmgmt -a build -p ferret 
 parsecmgmt -a build -p ferret -c gcc-hooks 
+parsecmgmt -a build -p netferret 
+parsecmgmt -a build -p netferret -c gcc-hooks 
+parsecmgmt -a build -p fluidanimate
 parsecmgmt -a build -p fluidanimate -c gcc-hooks 
+parsecmgmt -a build -p freqmine 
 parsecmgmt -a build -p freqmine -c gcc-hooks 
+parsecmgmt -a build -p streamcluster 
 parsecmgmt -a build -p streamcluster -c gcc-hooks
+parsecmgmt -a build -p netstreamcluster 
+parsecmgmt -a build -p netstreamcluster -c gcc-hooks
+parsecmgmt -a build -p swaptions 
 parsecmgmt -a build -p swaptions -c gcc-hooks 
+parsecmgmt -a build -p vips 
 parsecmgmt -a build -p vips -c gcc-hooks 
-parsecmgmt -a build -p x264 -c gcc-hooks 
-parsecmgmt -a build -p raytrace -c gcc-hooks 
+parsecmgmt -a build -p x264
+parsecmgmt -a build -p x264 -c gcc-hooks
+parsecmgmt -a build -p raytrace 
+parsecmgmt -a build -p raytrace -c gcc-hooks
