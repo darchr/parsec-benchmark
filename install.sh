73594e53d030795b95c7b2455965b83bdacae124
mahyarsamani <msamani@ucdavis.edu>
2020-02-22 19:28:02 -0800

    Updated install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+
 source env.sh
 parsecmgmt -a build -p blackscholes 
 parsecmgmt -a build -p blackscholes -c gcc-hooks 
@@ -29,11 +30,13 @@ parsecmgmt -a build -p vips
 parsecmgmt -a build -p vips -c gcc-hooks 
 parsecmgmt -a build -p x264
 parsecmgmt -a build -p x264 -c gcc-hooks
-echo "12345" | sudo chown gem5 -R /usr/local
+export PDIR="$(pwd)"
+export USR="$(whoami)"
+echo "12345" | sudo chown $USR -R /usr/local
 parsecmgmt -a build -p raytrace 
-cp -r /usr/local/bin /home/gem5/parsec-benchmark/pkgs/tools/cmake/inst/amd64-linux.gcc/
+cp -r /usr/local/bin $PDIR/pkgs/tools/cmake/inst/amd64-linux.gcc/
 parsecmgmt -a build -p raytrace
 parsecmgmt -a build -p raytrace -c gcc-hooks
-cp -r /usr/local/bin /home/gem5/parsec-benchmark/pkgs/tools/cmake/inst/amd64-linux.gcc-hooks/
+cp -r /usr/local/bin $PDIR/pkgs/tools/cmake/inst/amd64-linux.gcc-hooks/
 parsecmgmt -a build -p raytrace -c gcc-hooks
 echo "12345" | sudo chown root -R /usr/local
