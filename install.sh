9c94a17ba70ae071f84b4282d85c7243f90f3135
mahyarsamani <msamani@ucdavis.edu>
2020-03-04 17:07:50 -0800

    updated install.sh for installing raytrace.

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
-
 source env.sh
 parsecmgmt -a build -p blackscholes 
 parsecmgmt -a build -p blackscholes -c gcc-hooks 
@@ -30,11 +29,15 @@ parsecmgmt -a build -p vips
 parsecmgmt -a build -p vips -c gcc-hooks 
 parsecmgmt -a build -p x264
 parsecmgmt -a build -p x264 -c gcc-hooks
-echo "12345" | sudo chown gem5 -R /usr/local
+echo "12345" | sudo -S chown gem5 -R /usr/local/
+echo "12345" | sudo -S chgrp gem5 -R /usr/local/
 parsecmgmt -a build -p raytrace 
-cp -r /usr/local/bin /home/gem5/parsec-benchmark/pkgs/tools/cmake/inst/amd64-linux.gcc/
+cp -r /usr/local/bin/ /home/gem5/parsec-benchmark/pkgs/tools/cmake/inst/amd64-linux.gcc/
 parsecmgmt -a build -p raytrace
+cp -r /usr/local/bin/ /home/gem5/parsec-benchmark/pkgs/apps/raytrace/inst/amd64-linux.gcc/
 parsecmgmt -a build -p raytrace -c gcc-hooks
-cp -r /usr/local/bin /home/gem5/parsec-benchmark/pkgs/tools/cmake/inst/amd64-linux.gcc-hooks/
+cp -r /usr/local/bin/ /home/gem5/parsec-benchmark/pkgs/tools/cmake/inst/amd64-linux.gcc-hooks/
 parsecmgmt -a build -p raytrace -c gcc-hooks
-echo "12345" | sudo chown root -R /usr/local
+cp -r /usr/local/bin/ /home/gem5/parsec-benchmark/pkgs/apps/raytrace/inst/amd64-linux.gcc-hooks/
+echo "12345" | sudo -S chown root -R /usr/local/
+echo "12345" | sudo -S chgrp root -R /usr/local/
